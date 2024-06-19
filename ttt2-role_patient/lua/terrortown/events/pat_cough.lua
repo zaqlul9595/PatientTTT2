20723c5de4fc5d4b378ff946fbc05fc20016c2d2
milkwxter <ethan.r.bates2003@gmail.com>
2024-06-19 17:27:48 -0500

    Update pat_cough.lua

--- ttt2-role_patient/lua/terrortown/events/pat_cough.lua
+++ ttt2-role_patient/lua/terrortown/events/pat_cough.lua
@@ -19,11 +19,46 @@ end
 -- Function that gives sick traits to a player
 function makePlayerPatientSick(sickPlayer)
     print(tostring(sickPlayer) .. " BECAME SICK.")
+    sickPlayer:SetNWBool("patient_poisoned", true)
+end
+
+-- make the screen slightly yellow and blend some frames as a kind of "nausea" effect
+if CLIENT then
+    hook.Add("RenderScreenspaceEffects", "patient_infection_effects", function()
+        if LocalPlayer():GetNWBool("patient_poisoned", false) then
+            local colorModify = {
+	        [ "$pp_colour_addr" ] = 0.3,
+	        [ "$pp_colour_addg" ] = 0.3,
+	        [ "$pp_colour_addb" ] = 0,
+	        [ "$pp_colour_brightness" ] = 0,
+	        [ "$pp_colour_contrast" ] = 1,
+	        [ "$pp_colour_colour" ] = 1,
+	        [ "$pp_colour_mulr" ] = 0,
+	        [ "$pp_colour_mulg" ] = 0,
+                [ "$pp_colour_mulb" ] = 0
+            }
+            DrawColorModify(colorModify)
+            DrawMotionBlur(0.17, 0.65, 0.03)
+        end
+    end)
+end
+
+-- remove any poison effects on player death or round start
+if SERVER then
+    hook.Add("PostPlayerDeath", "patient_uninfection_effects_on_death", function(ply, infl, att)
+        ply:SetNWBool("patient_poisoned", false)
+    end)
+    hook.Add("TTTPrepareRound", "patient_uninfection_effects_on_prepare", function()
+        for i, j in pairs(player.GetAll()) do
+            j:SetNWBool("patient_poisoned", false)
+        end
+    end)
 end
 
 -- Function that gives immune traits to a player
 function makePlayerPatientImmune(sickPlayer)
     print(tostring(sickPlayer) .. " BECAME IMMUNE.")
+    sickPlayer:SetNWBool("patient_poisoned", false)
     if SERVER then
         sickPlayer:GiveItem("item_ttt_speedrun")
     end
